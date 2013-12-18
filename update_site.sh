02ff81c9b425ca042cd6172746c2c51f63ddd32e
agrasso <agrasso@princeton.edu>
2013-12-18 00:49:06 -0500

    now without bootstrap

--- update_site.sh
+++ update_site.sh
@@ -4,7 +4,8 @@ echo "Rendering templates..."
 python /Users/default/fun/sailing/render_templates.py
 echo "Done."
 echo "Rendering and compressing css files..."
-lessc --yui-compress /Users/default/fun/sailing/site/static/main.less /Users/default/fun/sailing/site/static/main.css
-echo "Uploading to server..."
-rsync -ahvP --delete /Users/default/fun/sailing agrasso@arizona.princeton.edu:/u/agrasso/public_html/sailing
+lessc --yui-compress /Users/default/fun/sailing/site/static/base.less /Users/default/fun/sailing/site/static/base.css
 echo "Done"
+# echo "Uploading to server..."
+#! rsync -ahvP --delete /Users/default/fun/sailing agrasso@arizona.princeton.edu:/u/agrasso/public_html/sailing
+# echo "Done"
