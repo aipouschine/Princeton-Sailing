8c581e9ff86306ea5bdad32c194fb8cbcf82eff2
agrasso <agrasso@princeton.edu>
2014-03-04 17:28:51 -0500

    new font, roster using bootstrap

--- render_templates.py
+++ render_templates.py
@@ -3,11 +3,19 @@ import json
 
 env = Environment(loader=FileSystemLoader('templates'))
 
+tabs = [('Home','index'),
+        ('Roster','roster'),
+        ('News','news'),
+        ('Alumni','alumni'),
+        ('Photos','photos'),
+        ('Schedule','schedule')
+        ]
+
 def make_index():
    template = env.get_template('index.html')
    with open('text/home_text.html','r') as f:
       home_text = f.read()
-   output = template.render(selected="home",home_text=home_text)
+   output = template.render(tabs=tabs,selected="index",home_text=home_text)
    with open('site/index.html','w') as f:
       f.write(output)
 
@@ -15,25 +23,25 @@ def make_roster():
    template = env.get_template('roster.html')
    with open('data/roster.json','r') as f:
       team = json.load(f)
-   output = template.render(selected="roster",team=team)
+   output = template.render(tabs=tabs,selected="roster",team=team)
    with open('site/roster.html','w') as f:
       f.write(output)
 
 def make_news():
    template = env.get_template('news.html')
-   output = template.render(selected="news")
+   output = template.render(tabs=tabs,selected="news")
    with open('site/news.html','w') as f:
       f.write(output)
 
 def make_alumni():
    template = env.get_template('alumni.html')
-   output = template.render(selected="alumni")
+   output = template.render(tabs=tabs,selected="alumni")
    with open('site/alumni.html','w') as f:
       f.write(output)
 
 def make_photos():
    template = env.get_template('photos.html')
-   output = template.render(selected="photos")
+   output = template.render(tabs=tabs,selected="photos")
    with open('site/photos.html','w') as f:
       f.write(output)
 
@@ -41,7 +49,7 @@ def make_schedule():
    template = env.get_template('schedule.html')
    with open('data/schedule.json','r') as f:
       schedule = json.load(f)
-   output = template.render(selected="schedule", schedule=schedule)
+   output = template.render(tabs=tabs,selected="schedule", schedule=schedule)
    with open('site/schedule.html','w') as f:
      f.write(output)
 
--- site/static/js/ps.js
+++ site/static/js/ps.js
@@ -1,7 +1,19 @@
 (function( ps, $, undefined) {
   var baseUrl = "static/bios"
+    , $activePopup
   ;
 
   ps.showBio = function(netid) {
-  }
+    $('#mask').removeClass('hidden');
+    $activePopup = $('#' + netid);
+    $activePopup.removeClass('hidden');
+    $('#mask').click(ps.closeBio);
+    $('.newsfeed').css('overflow','hidden');
+  };
+  ps.closeBio = function() {
+    $activePopup.addClass('hidden');
+    $('#mask').addClass('hidden');
+    $('.newsfeed').css('overflow','auto');
+  };
+  
 }(window.ps = window.ps || {}, jQuery))
