4bdcbdac1fc99cd4351d127df429313a63cac3ad
agrasso <agrasso@princeton.edu>
2015-01-02 16:44:21 -0500

    now deploying as sailing. probably final location.

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -ahvP --delete /Users/default/Sailing/Sailing_Site/site agrasso@arizona.princeton.edu:/u/agrasso/public_html/sailing
+rsync -ahvP --delete /Users/default/Sailing/Sailing_Site/site sailing@arizona.princeton.edu:/u/sailing/public_html
