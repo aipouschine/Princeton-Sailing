c97aa029c7efd0d4ee42e1dff023400797c26f62
agrasso <agrasso@princeton.edu>
2015-10-24 21:25:54 -0400

    no compress

--- update_site.sh
+++ update_site.sh
@@ -4,5 +4,5 @@ echo "Rendering templates..."
 python render_templates.py
 echo "Done."
 echo "Rendering and compressing css files..."
-lessc --yui-compress less/base.less site/static/css/base.css
+lessc less/base.less site/static/css/base.css
 echo "Done."
